52def3d4fbde0a543e20b14514f8d7732099e293
Jose D <joserafael161@hotmail.com>
2019-01-26 14:27:51 -0500

    Add splash

--- src/main/java/io/bobba/poc/BobbaEnvironment.java
+++ src/main/java/io/bobba/poc/BobbaEnvironment.java
@@ -1,6 +1,8 @@
 package io.bobba.poc;
 
 import io.bobba.poc.core.Game;
+import io.bobba.poc.misc.logging.LogLevel;
+import io.bobba.poc.misc.logging.Logging;
 
 public class BobbaEnvironment {
     private static BobbaEnvironment instance;
@@ -8,10 +10,22 @@ public class BobbaEnvironment {
     private Game game;
 
     private BobbaEnvironment() {
+    	System.out.println("");
+        System.out.println("|         |    |          o     ");
+        System.out.println("|---.,---.|---.|---.,---. .,---.");
+        System.out.println("|   ||   ||   ||   |,---| ||   |");
+        System.out.println("`---'`---'`---'`---'`---^o``---'");
+        System.out.println("Copyright (c) 2019 - relevance");
+        System.out.println();
+        Logging.getInstance().setLogLevel(LogLevel.Verbose);
+        
         this.game = new Game();
+        Logging.getInstance().writeLine("The environment has initialized successfully. Ready for connections.", LogLevel.Verbose);
+        
+        
     }
 
-    public static BobbaEnvironment getEnvironment() {
+    public static BobbaEnvironment getInstance() {
         return instance;
     }
 
